8800181dc09ab7dcc92ae108deff5777e6e8ceeb
ChoppaYagna <choppayagna4578@gmail.com>
2025-04-20 23:31:03 +0530

    Python

---
+++ Operators/Day-4(10).py
@@ -0,0 +1,7 @@
+d = int(input())
+n = int(input())
+
+########### Write your code below ###############
+res = ( d - n ) % 7
+# print what day it was n days before d
+print(res)
---
+++ Operators/Day-4(9).py
@@ -0,0 +1,9 @@
+n = int(input()) 
+
+########### Write your code below ###############
+# Find last digit of given number n
+ans = abs(n) % 10
+########### Write your code above ###############
+
+# Print ans
+print(ans)
